d37c07df9029199cec362e876d5506cdf6965fb4
Miguel Luque <miguel_luque27@hotmail.com>
2023-03-13 15:14:48 -0700

    commit

--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,6 +1,11 @@
 <div class="contenedor mensaje">
     <?php include_once __DIR__ .'/../templates/nombre-sitio.php'; ?>
     <div class="contenedor-sm">
-        <p class="descripcion-pagina">Hemos enviado las instrucciones para confirmar tu cuenta a tu email</p>
+        <p class="descripcion-pagina">En un momento serás redirigido para iniciar sesión...</p>
     </div> <!--.contenedor-sm -->
+    <script>
+        setTimeout(() => {
+            window.location.href = "/";
+        }, 3000);
+    </script>
 </div>
